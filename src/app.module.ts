1e9bf99206991c0870b4d98f58bc45ca2897ae59
vvmelnik <vvmelnik@quasarpro.ru>
2025-02-18 19:57:20 +0300

    feat: added base configuration and libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { IS_DEV_ENV } from '../libs/common/utils/is-dev.util';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: !IS_DEV_ENV,
+			isGlobal: true
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
